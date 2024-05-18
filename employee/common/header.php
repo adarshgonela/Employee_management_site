0e74ab73cd7173b9e23d3f87425fd99cc1ee2a2f
adarshgonela <adarshgonela@gmail.com>
2024-05-18 16:51:34 +0530

    dashboard fixed

---
+++ employee/common/header.php
@@ -0,0 +1,1 @@
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.3/dist/tailwind.min.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.3/dist/tailwind.min.css" rel="stylesheet">
+  <title>Document</title>
 </head>
 <body>
-    <?php echo "ASUS";  ?>  <br>
-    <?php echo "Hello";  ?>  <br>
-    <?php echo "RYZEN";  ?>  <br>
-  
+      <?php include ('employee/common/navbar.php'); ?>
+      <?php include ('employee/common/sidebar.php'); ?>
 </body>
 </html>
