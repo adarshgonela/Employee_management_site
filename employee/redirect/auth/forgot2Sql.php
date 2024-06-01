fb479923f27a0cb5125aed9d260f3ab1dfcad098
adarshgonela <adarshgonela@gmail.com>
2024-06-01 20:56:51 +0530

    forgot and reset password functionality done

--- employee/redirect/auth/forgot2Sql.php
+++ employee/redirect/auth/forgot2Sql.php
@@ -1,19 +1,18 @@
 <?php
 session_start();
 include('../conn.php');
-$email = $_SESSION['email'];
-
+$email1 = $_GET["email"];
 $password = $_REQUEST['password'];
 $cpassword = $_REQUEST['cpassword'];
 
-
 if ($password === $cpassword) {
-    $sql = "UPDATE users SET password='$password' WHERE email='$email'";
+    $sql = "UPDATE users SET password='$password' WHERE email='$email1'";
     $result = mysqli_query($conn, $sql);
     if ($result) {
         header("Location:../login.php ? password updated successfully");
         exit;
     }
 } else {
-    header("Location:../forgot2.php ? enter both password same");
+ 
+    header("Location:../forgot.php ? enter both password same");
 }
