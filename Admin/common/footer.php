b479a467af2507c8fd4b5b619f9596fdeb57e70f
adarshgonela <adarshgonela@gmail.com>
2024-06-03 13:27:47 +0530

    dropdown done

---
+++ Admin/common/footer.php
@@ -0,0 +1,2 @@
+   <script src="../assets/script.js">
+   </script>
---
+++ Admin/frontendpages/dropdown.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../assets/vendors/simple-line-icons/css/simple-line-icons.css">
+  <link rel="stylesheet" href="../assets/vendors/flag-icon-css/css/flag-icons.min.css">
+  <link rel="stylesheet" href="../assets/vendors/css/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="../assets/vendors/select2/select2.min.css">
+  <link rel="stylesheet" href="../assets/vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <!-- endinject -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="../assets/css/vertical-light-layout/style.css">
+  <!-- End layout styles -->
+</head>
+
+<body>
+  <?php
+
+  include('dropdownSql.php');
+  $ids = getIDs();
+  echo "hi";
+  ?>
+  <div class="col-12 grid-margin stretch-card">
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title">Basic form elements</h4>
+        <p class="card-description"> Basic form elements </p>
+        <form class="forms-sample" action="dropdownSql.php" method="post">
+          <div class="form-group">
+
+            <label for="exampleSelectid">ID</label>
+
+
+            <select class="form-select" id="exampleSelectid">
+              <?php
+              foreach ($ids as $id) {
+
+              ?>
+                <option><?php echo $id['user_id'] ?></option>
+              <?php } ?>
+            </select>
+
+          </div>
+          <div class="form-group">
+            <label for="exampleInputName1">Name</label>
+            <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail3">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword4">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label for="exampleSelectGender">Gender</label>
+            <select class="form-select" id="exampleSelectGender">
+              <option>Male</option>
+              <option>Female</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>File upload</label>
+            <input type="file" name="img[]" class="file-upload-default">
+            <div class="input-group col-xs-12">
+              <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+              <span class="input-group-append">
+                <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+              </span>
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputCity1">City</label>
+            <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
+          </div>
+          <div class="form-group">
+            <label for="exampleTextarea1">Textarea</label>
+            <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary me-2">Submit</button>
+          <button class="btn btn-light">Cancel</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
