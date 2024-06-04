1a06fa46fd9c6c5e14d79c41c75361de188c4cbc
adarshgonela <adarshgonela@gmail.com>
2024-06-04 22:10:09 +0530

    logo

--- Admin/common/sidebar.php
+++ Admin/common/sidebar.php
@@ -46,7 +46,7 @@
                   <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"></path></svg>
                 
                  </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Work Schdule</span>
+                <span class="ml-2 text-sm tracking-wide truncate">Work Schedule</span>
                 
               </a>
             </li>
