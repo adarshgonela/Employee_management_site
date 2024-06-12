c41d15bd2a656b13356cf8b5550fb96f2325df6f
5pavithra <pavithrachunduri1@gmail.com>
2024-06-12 12:29:49 +0530

    .

--- employee/redirect/conn.php
+++ employee/redirect/conn.php
@@ -13,17 +13,6 @@ if (!$conn) {
 	exit();
 }
 
-<<<<<<< HEAD
-// function input($key) {
-// 	global $_REQUEST;
-// 	if(isset($_REQUEST[$key]) && $_REQUEST[$key] && trim($_REQUEST[$key])!=""){
-// 		return trim($_REQUEST[$key]);
-// 	}
-// 	return null;
-// }
-=======
-
->>>>>>> e3b6cb6b0131a62f0c5471263d2ab80f90da806e
 
 $logoutLimit = 10;
 $present = date('H:i');
